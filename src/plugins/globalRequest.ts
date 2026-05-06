b52d025405b9db26e569965499d9cdc823c6a34f
nomo1024 <3529009928@qq.com>
2026-05-06 14:21:14 +0800

    首次提交

--- src/plugins/globalRequest.ts
+++ src/plugins/globalRequest.ts
@@ -12,7 +12,7 @@ import {stringify} from "querystring";
  */
 const request = extend({
   credentials: 'include', // 默认请求是否带上cookie
-  prefix: process.env.NODE_ENV === 'production' ? 'http://user-backend.code-nav.cn' : 'http://localhost:8080'
+  prefix: process.env.NODE_ENV === 'production' ? 'http://192.168.23.128:8080' : 'http://localhost:8080'
   // requestType: 'form',
 });
 
